3b937083e9f4971ab556c2c69c4874aacb3dc3cc
LewBlu <lewis.blundell.development@gmail.com>
2024-06-15 20:52:33 +0100

    Created locations page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { ActivityComponent } from './pages/activity/activity.component';
 import { authGuard } from './auth/auth.guard';
+import { LocationsComponent } from './pages/locations/locations.component';
 
 export const routes: Routes = [
     { path: '', component: ActivityComponent, canActivate: [authGuard]},
+    { path: 'locations', component: LocationsComponent, canActivate: [authGuard]},
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
 ];
