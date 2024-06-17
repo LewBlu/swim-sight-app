0ea1496a909b9bea64204213d9ded8c1daf7031c
LewBlu <lewis.blundell.development@gmail.com>
2024-06-17 23:31:18 +0100

    Fetch locations from api

--- src/app/pages/locations/locations.component.ts
+++ src/app/pages/locations/locations.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LocationCardComponent } from './location-card/location-card.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-locations',
@@ -8,10 +9,14 @@ import { LocationCardComponent } from './location-card/location-card.component';
   templateUrl: './locations.component.html',
   styleUrl: './locations.component.scss'
 })
-export class LocationsComponent {
-  public locations: {name: string, lat: string, long:string}[] = [
-    { name: 'Lingmere Fishery', lat: '53.41083228882654', long: '-3.1259834102978545' },
-    { name: 'Larton Fishery', lat: '53.374738343707016', long: '-3.144247219954584' },
-    { name: 'Claremont Farm', lat: '53.33396825266775', long: '-3.0144821131723125' },
-  ];
+export class LocationsComponent implements OnInit {
+  public locations: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('api/location').subscribe(locations => {
+      this.locations = locations;
+    });
+  }
 }
